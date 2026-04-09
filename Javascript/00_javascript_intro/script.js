d5987e5762e09a429d8ebfc8d5093e1220a74b55
Zeroo2002 <u20ece1011@cit.ac.in>
2026-04-09 09:30:20 +0530

    started javascript

---
+++ Javascript/00_javascript_intro/script.js
@@ -0,0 +1,17 @@
+alert("hello world");
+
+console.log("the code is running...1..2..");
+console.log("the code has successfully executed");
+
+
+var a = prompt("Enter your number");
+console.log("Your number is " + a);
+
+var isTrue = confirm("Are you sure you want to blast your computer?");
+
+if(isTrue){
+    console.log("Blasting your computer....");
+}
+else{
+    console.log("Clearing...");
+}
